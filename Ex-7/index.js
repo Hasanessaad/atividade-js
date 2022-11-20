99d94dadeaec5d3ca015b5fcafd191ca855ac0d1
Hasanessaad <hasancayedessaad@gmail.com>
2022-11-20 16:11:04 -0300

    Co-authored-by: WanderfullMystc <WanderfullMystc@users.noreply.github.com>

---
+++ Ex-7/index.js
@@ -0,0 +1,18 @@
+let number;
+let cost;
+
+number = window.prompt("indicate the quantity you desire:");
+
+document.getElementById('quantity').innerHTML = (number);
+
+if(number<12){
+
+    cost = number * 1.30;
+    document.getElementById('quantity_less').innerHTML = (cost);
+
+}else if (number>=12){
+
+    cost = number * 1;
+    document.getElementById('quantity_more').innerHTML = (cost);
+
+}
